f8bee7bb13e22ca56db2f616fcea0a7a6112593f
CaSilicate <casilicate@outlook.com>
2025-09-20 17:59:12 +0800

    Sep.20 2025 17:59

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,109 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+'use client';
+import '@ant-design/v5-patch-for-react-19';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Card, Space, Row, Tooltip, Typography, Skeleton, notification, message } from "antd";
+const { Title, Text } = Typography;
+
+import { ReactElement } from 'react';
+import Link from "next/link";
+import { Color } from 'antd/es/color-picker';
+export default function App() {
+  const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+  const [dataaa,setDataaa] = useState(null);
+  const n = useSearchParams().get("name");
+  const [cards,setCards] = useState<ReactElement[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  function openNotification(type, msg) {
+    notification[type]({
+      message: msg,
+    });
+  };
+
+  async function f() {
+    try{
+      if(!n) {
+        
+      }
+      console.log(n);
+      const res = await fetch(`${API_BASE}/list`)
+      const j = await res.json();
+      const d = j.namelist;
+
+      const result: ReactElement[] = [];
+      for (const [index, item] of d.entries()) {
+        console.log(item);
+        const cres = await fetch(`${API_BASE}/serverod/${item}`)
+        const cj = await cres.json();
+        console.log(cj)
+        const clatency = cj.data[0].latency;
+        const cplayer = cj.data[0].player;
+        const cpl = cj.data[0].playerlist
+        var ccl = "#fff";
+        if (clatency > 0 && clatency < 150) {
+          ccl = "#90ee90"
+        } else if (clatency >= 150) {
+          ccl = "#ff00ff"
+        }
+        else {
+          ccl = "#ff0000"
+        }
+
+        result.push(
+          <Link href={`/server/?name=${item}`}>
+            <Card key={index} /*extra={<Link href={`/server/?name=${item}`}>More</Link>}*/ title={`Server: ${item}`} style={{
+              width: 300,
+              boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)"
+            }}>
+              <p>
+                当前延迟: <Text strong style={{color: ccl}}>{clatency}</Text>
+              </p>
+              <Tooltip title={cpl}>
+                <p>当前玩家数量: <Text strong>{cplayer}</Text></p>
+              </Tooltip>
+            </Card>
+          </Link>
+
+        )
+        setLoading(false);
+      }
+      setCards(result);
+    } catch(error) {
+      openNotification('error',error.message);
+    }
+  }
+  function renderSkeletons() {
+    return (
+      <Skeleton/>
+    )
+  }
+  useEffect(() => {
+    f();
+    const timer = setInterval(f, 3000);
+    return () => clearInterval(timer);
+  },[])
+  /*return (
+  <div>
+    <Row gutter={[16, 16]}>
+      <Space>
+        {loading ? renderSkeletons() : cards}
+        <Skeleton active />
+      </Space>
+    </Row>
+  </div>
+  );*/
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  <div>
+    <Title>Minecraft 服务器监控器</Title>
+    {loading ? renderSkeletons() : (
+      <Row gutter={[16, 16]}>
+        <Space>
+          {loading ? renderSkeletons() : cards}
+        </Space>
+      </Row>
+    )}
+  </div>
   );
 }
---
+++ src/app/server/page.tsx
@@ -0,0 +1,67 @@
+
+'use client';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Table, Typography, Skeleton } from "antd";
+const { Title } = Typography;
+
+import { ReactElement } from 'react';
+export default function App() {
+  const [dataaa,setDataaa] = useState(null);
+  const n = useSearchParams().get("name");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  async function rd() {
+    if(!n) {
+      console.log("111");
+    }
+    console.log(n);
+    const res = await fetch(`http://127.0.0.1:18650/api/servers/${n}`)
+    //console.log(res.json())
+    const j = await res.json();
+    const d = j.data;
+    const columns = [
+      {
+        title: "Time",
+        dataIndex: "timestamp",
+        key: "timestamp"
+      },
+      {
+        title: "Latency",
+        dataIndex: "latency",
+        key: "latency"
+      },
+      {
+        title: "Player",
+        dataIndex: "player",
+        key: "player"
+      }
+    ]
+    function r() {
+      return (
+        <>
+          <Title>
+            当前服务器: {n}
+          </Title>
+          <Table dataSource={d} columns={columns} rowKey={(record) => record.timestamp}/>
+        </>
+
+    );
+    };
+    setDataaa(r());
+    setLoading(false);
+  }
+  function renderSkeletons() {
+    return (
+      <Skeleton/>
+    )
+  }
+  useEffect(() => {
+    rd();
+  },[])
+  return (
+  <div>
+      {loading ? renderSkeletons() : dataaa}
+  </div>
+  );
+}
