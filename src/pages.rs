a2c45981b31622d4e43704c31f6bffd7f386bd29
CaSilicate <casilicate@outlook.com>
2025-10-07 22:00:25 +0800

    feat: Realtime update

--- src/pages.rs
+++ src/pages.rs
@@ -4,6 +4,7 @@ use serde::Deserialize;
 use serde_json;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
+use gloo_timers::future::TimeoutFuture;
 
 use crate::utils;
 #[derive(Deserialize, Debug, Clone)]
@@ -46,90 +47,93 @@ pub fn home() -> Html {
         let r = r.clone();
         use_effect_with((), move |_| {
             spawn_local(async move {
-                let api_base = utils::get_api_base().await;
+                    let api_base = utils::get_api_base().await;
 
-                let resp = reqwasm::http::Request::get(&format!("{}/list", api_base))
-                    .send()
-                    .await
-                    .unwrap();
-                let data = resp.text().await.unwrap();
-                let json: ListResp = serde_json::from_str(&data).unwrap();
-                let nl = json.namelist;
-                r.set(html!{ <div>{ "Fetching status data" }</div>});
-                /*let mut sl: Vec<SingleServerResp> = Vec::new();
-                for i in nl.iter() {
-                    let sresp = reqwasm::http::Request::get(&format!(
-                        "{}/serverod/{}",
-                        api_base,
-                        *i
-                    ))
-                    .send()
-                    .await
-                    .unwrap();
-                    let sdata = sresp.text().await.unwrap();
-                    let sjson: SingleServerResp = serde_json::from_str(&sdata).unwrap();
-                    sl.push(sjson);
-                }*/
-                let mut cfutures = Vec::new();
-                for i in nl.iter() {
-                    let id = (*i).clone();
-                    let api_base = api_base.clone();
-                    let sreqw = async move {
-                        serde_json::from_str::<SingleServerResp>(
-                            &reqwasm::http::Request::get(&format!("{}/serverod/{}", api_base, id))
-                                .send()
-                                .await
+                    let resp = reqwasm::http::Request::get(&format!("{}/list", api_base))
+                        .send()
+                        .await
+                        .unwrap();
+                    let data = resp.text().await.unwrap();
+                    let json: ListResp = serde_json::from_str(&data).unwrap();
+                    let nl = json.namelist;
+                    r.set(html!{ <div>{ "Fetching status data" }</div>});
+                    /*let mut sl: Vec<SingleServerResp> = Vec::new();
+                    for i in nl.iter() {
+                        let sresp = reqwasm::http::Request::get(&format!(
+                            "{}/serverod/{}",
+                            api_base,
+                            *i
+                        ))
+                        .send()
+                        .await
+                        .unwrap();
+                        let sdata = sresp.text().await.unwrap();
+                        let sjson: SingleServerResp = serde_json::from_str(&sdata).unwrap();
+                        sl.push(sjson);
+                    }*/
+                    loop {
+                        let mut cfutures = Vec::new();
+                        for i in nl.iter() {
+                            let id = (*i).clone();
+                            let api_base = api_base.clone();
+                            let sreqw = async move {
+                                serde_json::from_str::<SingleServerResp>(
+                                    &reqwasm::http::Request::get(&format!("{}/serverod/{}", api_base, id))
+                                        .send()
+                                        .await
+                                        .unwrap()
+                                        .text()
+                                        .await
+                                        .unwrap(),
+                                )
                                 .unwrap()
-                                .text()
-                                .await
-                                .unwrap(),
-                        )
-                        .unwrap()
-                    };
-                    cfutures.push(sreqw);
-                }
-                let all_servers: Vec<SingleServerResp> = futures::future::join_all(cfutures).await;
-
-                r.set(html!{
-                    <div class="d-flex flex-wrap">
-                        {
-                            for (0..all_servers.len()).map(|i| {
-                                html!{
-                                    <a href={ format!("server/{}",&nl[i])} class="text-decoration-none">
-                                        <div class="card m-2" style="width: 18rem;">
-                                            //<div class="card-body"><a href={ format!("server/{}",&nl[i])} class="btn btn-primary">{&nl[i]} {" : "} {&ll[i]}</a></div>
-                                            <div class="card-body">
-                                                <h5>{ format!("Server: {}", all_servers[i].label) }</h5>
-                                                <h6>{ format!("Server id: {}", nl[i]) }</h6>
-                                                <p>{ /*format!("Current latency: {}", sl[i].data[0].latency)*/
-                                                    (|| {
-                                                        let clatency = all_servers[i].data[0].latency;
-                                                        if clatency == -1 {
-                                                            return html!{
-                                                                <>
-                                                                    <span>{ "Current latency: " }</span>
-                                                                    <span style={ format!("color: #{}","ff0000") }>{ clatency }</span>
-                                                                </>
-                                                            }
-                                                        }
-                                                        let cpercentage = (clatency) as f64 / 1000_f64;
-                                                        let color = utils::percent_color((144, 238, 144), (255, 0, 0), cpercentage);
-                                                        return html!{
-                                                            <>
-                                                                <span>{ "Current latency: " }</span>
-                                                                <span style={ format!("color: #{}",color) }>{ clatency }</span>
-                                                            </>
-                                                        }
-                                                    })()
-                                                }</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                }
-                            })
+                            };
+                            cfutures.push(sreqw);
                         }
-                    </div>
-                });
+                        let all_servers: Vec<SingleServerResp> = futures::future::join_all(cfutures).await;
+
+                        r.set(html!{
+                            <div class="d-flex flex-wrap">
+                                {
+                                    for (0..all_servers.len()).map(|i| {
+                                        html!{
+                                            <a href={ format!("server/{}",&nl[i])} class="text-decoration-none">
+                                                <div class="card m-2" style="width: 18rem;">
+                                                    //<div class="card-body"><a href={ format!("server/{}",&nl[i])} class="btn btn-primary">{&nl[i]} {" : "} {&ll[i]}</a></div>
+                                                    <div class="card-body">
+                                                        <h5>{ format!("Server: {}", all_servers[i].label) }</h5>
+                                                        <h6>{ format!("Server id: {}", nl[i]) }</h6>
+                                                        <p>{ /*format!("Current latency: {}", sl[i].data[0].latency)*/
+                                                            (|| {
+                                                                let clatency = all_servers[i].data[0].latency;
+                                                                if clatency == -1 {
+                                                                    return html!{
+                                                                        <>
+                                                                            <span>{ "Current latency: " }</span>
+                                                                            <span style={ format!("color: #{}","ff0000") }>{ clatency }</span>
+                                                                        </>
+                                                                    }
+                                                                }
+                                                                let cpercentage = (clatency) as f64 / 1000_f64;
+                                                                let color = utils::percent_color((144, 238, 144), (255, 0, 0), cpercentage);
+                                                                return html!{
+                                                                    <>
+                                                                        <span>{ "Current latency: " }</span>
+                                                                        <span style={ format!("color: #{}",color) }>{ clatency }</span>
+                                                                    </>
+                                                                }
+                                                            })()
+                                                        }</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        }
+                                    })
+                                }
+                            </div>
+                        });
+                        TimeoutFuture::new(5000).await;
+                    }
             });
             || ()
         });
